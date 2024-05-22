95068cb7beffa9ba72dafb6534b0b0ff24678aa1
KhaleDM1198 <108348779+KhaleDM1198@users.noreply.github.com>
2024-05-22 05:49:56 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,20 @@
-rom telebot import TeleBot
+from telebot import TeleBot
 from keep_alive import keep_alive
 import requests
 import time
 
 keep_alive()
 # Initialize the Telegram bot
-bot = TeleBot('6552877762:AAGoZjusMeGCJHe8istpXW8u5AFMhhFFL9c')
+bot = TeleBot('YOUR_TELEGRAM_BOT_TOKEN_HERE')
 file_path = "doneee.csv"
 
 # Handle the '/start' command
-
-
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
     chat_id = message.chat.id
     user_name = message.chat.first_name if message.chat.first_name else "User"
     message = f"Welcome, {user_name}.\nServer is running"
     bot.send_message(chat_id, message, parse_mode="HTML")
-
 
 # Handle the '/spamcall' command
 @bot.message_handler(commands=['spamcall'])
@@ -26,7 +23,6 @@ def spam_call(message):
     with open(file_path, 'r') as file:
         phone_numbers = file.read().splitlines()
         for phone_number in phone_numbers:
-            # make_call(number)
             url = "https://sms-call.vercel.app/api/call"
             payload = {
                 "phone": phone_number
@@ -36,38 +32,30 @@ def spam_call(message):
                 if response.status_code == 200:
                     message = f"Call made to {phone_number} successfully!"
                     bot.send_message(chat_id, message, parse_mode="HTML")
-
                 else:
-                    return f"Failed to make call to {phone_number}. Status code: {response.status_code}"
+                    message = f"Failed to make call to {phone_number}. Status code: {response.status_code}"
+                    bot.send_message(chat_id, message, parse_mode="HTML")
             except requests.exceptions.RequestException as e:
-                print(f"Error occurred while making call to {
-                      phone_number}: {e}")
+                print(f"Error occurred while making call to {phone_number}: {e}")
             time.sleep(5)
 
-
+# Handle the '/spammsg' command
 @bot.message_handler(commands=['spammsg'])
-def spam_call(message):
+def spam_message(message):
     chat_id = message.chat.id
     with open(file_path, 'r') as file:
         phone_numbers = file.read().splitlines()
         for phone_number in phone_numbers:
             try:
-                response2 = requests.get(
-                    f'https://spamwhats.vercel.app/send_spam?number={phone_number}')
-                if response2.status_code == 200:
-                    message = f"Whats Message made to {
-                        phone_number} successfully!"
+                response = requests.get(f'https://spamwhats.vercel.app/send_spam?number={phone_number}')
+                if response.status_code == 200:
+                    message = f"Whats Message sent to {phone_number} successfully!"
                     bot.send_message(chat_id, message, parse_mode="HTML")
-
                 else:
-                    message = f"Failed to make Whats Message to {
-                        phone_number}. Status code: {response2.status_code}"
+                    message = f"Failed to send Whats Message to {phone_number}. Status code: {response.status_code}"
                     bot.send_message(chat_id, message, parse_mode="HTML")
-
             except requests.exceptions.RequestException as e:
-                print(f"Error occurred while making Whats Message to {
-                      phone_number}: {e}")
+                print(f"Error occurred while sending Whats Message to {phone_number}: {e}")
             time.sleep(5)
-
 
 bot.polling()
