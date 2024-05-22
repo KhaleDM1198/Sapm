41ff7525e47bdd51b5373eb62fd26b23d01adf69
KhaleDM1198 <108348779+KhaleDM1198@users.noreply.github.com>
2024-05-22 05:28:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ import telebot
 import requests
 import time
 from keep_alive import keep_alive
+
 keep_alive()
 
-sendToId=-1002041695132
+sendToId = -1002041695132
+
 def fetch_phone_numbers():
     try:
         response = requests.get("https://pastebin.com/raw/BdJYWtGw")
@@ -17,11 +19,11 @@ def fetch_phone_numbers():
 phone_numbers_string = fetch_phone_numbers()
 phone_numbers_list = phone_numbers_string.strip().split('\n')
 
-
 # Initialize your Telegram bot with the token you obtained from BotFather
 bot = telebot.TeleBot("6552877762:AAGoZjusMeGCJHe8istpXW8u5AFMhhFFL9c")
 running = True
-def make_call(phone_number,message,msg):
+
+def make_call(phone_number, message, msg):
     global sendToId
     url = "https://sms-call.vercel.app/api/call"
     payload = {
@@ -29,41 +31,34 @@ def make_call(phone_number,message,msg):
     }
     try:
         response = requests.post(url, json=payload)
-        if response.json()['message'] == 'Sent':
+        response_json = response.json()
+        if response_json.get('message') == 'Sent':
             print(f"Call made to {phone_number} successfully!")
-            # bot.send_message(sendToId, f"Calls sent successfully to {phone_number}")
-            bot.edit_message_text(chat_id=msg.chat.id, message_id=msg.message_id, text= f"Calls sent successfully to {phone_number}")
-
+            bot.edit_message_text(chat_id=msg.chat.id, message_id=msg.message_id, text=f"Calls sent successfully to {phone_number}")
         else:
-            print(f"Failed to make call to {phone_number}. Status code: {response.status_code}")
-            # bot.send_message(sendToId, f"Failed to make call to {phone_number}. error is: {response.json()['message']}")
-            bot.edit_message_text(chat_id=msg.chat.id, message_id=msg.message_id, text= f"Failed to make call to {phone_number}. error is: {response.json()['message']}")
-
+            print(f"Failed to make call to {phone_number}. error is: {response_json.get('message')}")
+            bot.edit_message_text(chat_id=msg.chat.id, message_id=msg.message_id, text=f"Failed to make call to {phone_number}. error is: {response_json.get('message')}")
     except requests.exceptions.RequestException as e:
         print(f"Error occurred while making call to {phone_number}: {e}")
-        # bot.send_message(sendToId, f"Error occurred while making call to {phone_number}: {e}")
-        bot.edit_message_text(chat_id=msg.chat.id, message_id=msg.message_id, text= f"Error occurred while making call to {phone_number}: {e}")
+        bot.edit_message_text(chat_id=msg.chat.id, message_id=msg.message_id, text=f"Error occurred while making call to {phone_number}: {e}")
 
-
-def make_calls(phone_numbers,message,msg):
+def make_calls(phone_numbers, message, msg):
     global running
     for number in phone_numbers:
         if not running:
             break  
-        make_call(number,message,msg)
+        make_call(number, message, msg)
         time.sleep(5)
-        
 
 # Handle "/send" command from Telegram
 @bot.message_handler(commands=['send'])
-def send_messages(message):
+def handle_send_command(message):
     phone_numbers_string = fetch_phone_numbers()
     phone_numbers_list = phone_numbers_string.strip().split('\n')
-    # Example list of phone numbers
-    msg=bot.reply_to(message, "Sending......")
+    msg = bot.reply_to(message, "Sending......")
     global running
     running = True
-    make_calls(phone_numbers_list,message,msg)
+    make_calls(phone_numbers_list, message, msg)
     bot.reply_to(message, "Done sent calls successfully!")
 
 @bot.message_handler(commands=['whatsapp'])
@@ -73,28 +68,25 @@ def spam_call(message):
     global sendToId
     global running
     running = True
-    chat_id =sendToId
+    chat_id = sendToId
     for phone_number in phone_numbers_list:
         if not running:
             break  
         try:
-            response2 = requests.get(
-                f'https://spamwhats.vercel.app/send_spam?number={phone_number}')
+            response2 = requests.get(f'https://spamwhats.vercel.app/send_spam?number={phone_number}')
             if response2.status_code == 200:
-                message = f"Whats Message made to {phone_number} successfully!"
-                bot.send_message(chat_id, message, parse_mode="HTML")
-
+                msg = f"Whats Message made to {phone_number} successfully!"
+                bot.send_message(chat_id, msg, parse_mode="HTML")
             else:
-                message = f"Failed to make Whats Message to {phone_number}. Status code: {response2.status_code}"
-                bot.send_message(chat_id, message, parse_mode="HTML")
-
+                msg = f"Failed to make Whats Message to {phone_number}. Status code: {response2.status_code}"
+                bot.send_message(chat_id, msg, parse_mode="HTML")
         except requests.exceptions.RequestException as e:
             print(f"Error occurred while making Whats Message to {phone_number}: {e}")
         time.sleep(5)
     bot.send_message(chat_id, "Done Send Spam", parse_mode="HTML")
-    
+
 @bot.message_handler(commands=['start'])
-def send_messages(message):
+def start_bot(message):
     global sendToId
     bot.send_message(sendToId, "/send دوس هنا عشان تبعت المكالمات ")
 
@@ -103,23 +95,20 @@ def stop_messages(message):
     global running
     running = False
     bot.reply_to(message, "Send Call stopped!")
+
 @bot.message_handler(commands=['usf'])
-
-def ChangeSendTo(message):
+def change_send_to(message):
     bot.reply_to(message, "ابعت 1 عشان ترجع لوضع الجروب")
-    bot.register_next_step_handler(message, handelSwitch)
-    
+    bot.register_next_step_handler(message, handle_switch)
 
-   
-def handelSwitch(message):
+def handle_switch(message):
     global sendToId
-    if message.text=="1":
-        sendToId=-1002041695132
-        bot.reply_to(message, "Groub Now")
+    if message.text == "1":
+        sendToId = -1002041695132
+        bot.reply_to(message, "Group Now")
     else:
-        sendToId=1098317745
+        sendToId = 1098317745
         bot.reply_to(message, "@usfnassr Now")
-
 
 # Start the bot
 bot.infinity_polling()
