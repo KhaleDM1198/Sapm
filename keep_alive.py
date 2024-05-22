8349621f081896e9aa829697abd1f458465d8591
KhaleDM1198 <108348779+KhaleDM1198@users.noreply.github.com>
2024-05-22 05:29:40 +0300

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,5 +1,4 @@
-rom flask import Flask,render_template
-from threading import Thread
+from flask import Flask
 
 app = Flask(__name__)
 
@@ -7,9 +6,10 @@ app = Flask(__name__)
 def index():
     return "Alive"
 
-def run():
-  app.run(host='0.0.0.0',port=8080)
+def keep_alive():
+    app.run(debug=False, host='0.0.0.0', port=8080)
 
-def keep_alive():  
-    t = Thread(target=run)
-    t.start()
+# If this script is run directly, start the Flask server
+if __name__ == "__main__":
+    keep_alive()
+
